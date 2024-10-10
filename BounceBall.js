3b240fdd6df1c55bdbced4dbabb462daccd45f84
Brilian-Maker <134251637+Brilian-Maker@users.noreply.github.com>
2024-10-10 11:18:04 +0700

    Add files via upload

---
+++ BounceBall.js
@@ -0,0 +1,23 @@
+let x = 320;
+let y = 180;
+
+let xspeed = 20;
+let yspeed = 2;
+
+let r = 30;
+function setup() {
+  createCanvas(640, 360);
+}
+
+function draw() {
+  background(0);
+  ellipse(x, y, r*2, r*2);
+  x += xspeed;
+  y += xspeed;
+  if (x > width - r || x < r){
+    xspeed = -xspeed;
+  }
+  if (y > height - r || y < r) {
+    yspeed = -yspeed;
+  }
+}
